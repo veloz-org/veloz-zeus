40eb6fe17fec2b54b33701458423324adec193f4
Benrobo <alumonabenaiah71@gmail.com>
2023-12-11 14:31:59 +0100

    Remove unused files and update dependencies

---
+++ src/config/font.ts
@@ -0,0 +1,33 @@
+import { Poppins } from "next/font/google";
+
+export const ppReg = Poppins({
+  subsets: ["latin"],
+  variable: "--font-ppReg",
+  weight: ["400"],
+});
+
+export const ppL = Poppins({
+  subsets: ["latin"],
+  variable: "--font-ppL",
+  weight: ["300"],
+});
+
+// bold poppins
+export const ppB = Poppins({
+  subsets: ["latin"],
+  variable: "--font-ppB",
+  weight: ["600"],
+});
+
+// extra bold
+export const ppSB = Poppins({
+  subsets: ["latin"],
+  variable: "--font-ppSB",
+  weight: ["600"],
+});
+
+export const ppEB = Poppins({
+  subsets: ["latin"],
+  variable: "--font-ppEB",
+  weight: ["900"],
+});
