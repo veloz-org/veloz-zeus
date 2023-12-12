197900baa77a3b8c84b69035891808233d09dc58
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 11:23:10 +0100

    updated and added major changes

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -3,13 +3,14 @@ import {
   FlexColStart,
   FlexColStartCenter,
 } from "@/components/Flex";
-import CredentialsAuth from "@/modules/auth/components/CredentialsAuth";
+import CredentialsAuth from "@/modules/auth/components/nextAuth/CredentialsAuth";
 import React from "react";
 
 function AuthPage() {
   return (
     <FlexColCenter className="w-full h-screen bg-white-300 dark:bg-dark-100">
       <FlexColStart className="w-full min-w-[350px] max-w-[400px] bg-white-100 ">
+        {/* Replace your prefer auth component */}
         <CredentialsAuth />
       </FlexColStart>
     </FlexColCenter>
