26a65b8af5e15d277b49f8e0f7e004d8c4a5099c
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 18:27:31 +0100

    Refactor code and added major changes

---
+++ src/modules/protectedRoutes/components/DashboardLayout.tsx
@@ -0,0 +1,47 @@
+"use client";
+import React, { useContext, useEffect, useState } from "react";
+import { twMerge } from "tailwind-merge";
+import SideBar from "@/modules/protectedRoutes/components/Navigations/Sidebar";
+import { LayoutContext } from "@/context/LayoutContext";
+import TopBar from "./Navigations/TopBar";
+import { useSession } from "next-auth/react";
+
+interface DashboardLayoutProps {
+  children?: React.ReactNode;
+  className?: React.ComponentProps<"div">["className"];
+  activePage: string;
+}
+
+function DashboardLayout({
+  children,
+  activePage,
+  className,
+}: DashboardLayoutProps) {
+  const { setActivePage } = useContext(LayoutContext);
+  const { status } = useSession();
+
+  useEffect(() => {
+    activePage && setActivePage(activePage);
+  }, [activePage]);
+
+  if (status === "loading" || status === "unauthenticated") return null;
+
+  return (
+    <div
+      className={twMerge(
+        "w-full relative h-screen overflow-y-auto hideScrollBar bg-white-200 dark:bg-dark-100",
+        className
+      )}
+    >
+      <div className="w-full h-screen flex">
+        <SideBar activePage={activePage as string} />
+        <div className="w-full z-upper relative  overflow-hidden">
+          <TopBar />
+          {children}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DashboardLayout;
