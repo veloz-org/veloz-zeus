a86513fc2a636862ddcf0e39937bb8ae84e2034d
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 18:27:34 +0100

    Refactor code and added major changes

---
+++ src/modules/protectedRoutes/components/Navigations/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+interface SidebarProps {
+  activePage: string;
+}
+
+function Sidebar({ activePage }: SidebarProps) {
+  return <div>Sidebar</div>;
+}
+
+export default Sidebar;
