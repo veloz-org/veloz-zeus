5b280810660a1bf998559dfd0a810ed1f3b81deb
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 18:27:16 +0100

    Refactor code and added major changes

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,12 +1,19 @@
+"use client";
 import {
   FlexColCenter,
   FlexColStart,
   FlexColStartCenter,
 } from "@/components/Flex";
+import { withoutAuth } from "@/lib/helpers";
 import CredentialsAuth from "@/modules/auth/components/nextAuth/CredentialsAuth";
+import { useSession } from "next-auth/react";
 import React from "react";
 
 function AuthPage() {
+  const { status } = useSession();
+
+  if (status === "loading" || status === "unauthenticated") return null;
+
   return (
     <FlexColCenter className="w-full h-screen bg-white-300 dark:bg-dark-100">
       <FlexColStart className="w-full min-w-[350px] max-w-[400px] bg-white-100 ">
@@ -17,4 +24,5 @@ function AuthPage() {
   );
 }
 
-export default AuthPage;
+// prevent user to access this page if they are already logged in
+export default withoutAuth(AuthPage);
