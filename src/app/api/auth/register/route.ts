a14885f159f93738d9cb4636da395e01abd5a7d2
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 18:27:08 +0100

    Refactor code and added major changes

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -1,7 +1,9 @@
 import { NextRequest } from "next/server";
 import UserController from "../../controllers/user.controller";
+import CatchError from "../../utils/_error";
 
 const userController = new UserController();
 
-export const POST = async (req: NextRequest) =>
-  await userController.register(req);
+export const POST = CatchError(
+  async (req: NextRequest) => await userController.register(req)
+);
