66a233edd470f74ea8c51e626300bd596f3e094c
Benrobo <alumonabenaiah71@gmail.com>
2023-12-12 18:27:27 +0100

    Refactor code and added major changes

--- src/http/requests.ts
+++ src/http/requests.ts
@@ -5,3 +5,9 @@ export const registerUsers = async (data: any) => {
   const resp = await $axios.post("/auth/register", data);
   return resp.data;
 };
+
+// fetch users info
+export const getUser = async () => {
+  const req = await $axios.get("/user");
+  return req.data;
+};
