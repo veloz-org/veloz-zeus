455f737924c2cc9ce181ca0063f2159d62225356
Benrobo <alumonabenaiah71@gmail.com>
2023-12-13 08:28:11 +0100

    Add number formatting functions to utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,15 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+// format number to 100, 10k, 1M, 1B, 1T, etc
+export function formatNumber(number: number) {
+  // International number formatting
+  let formatter = Intl.NumberFormat("en", { notation: "compact" });
+  return formatter.format(number);
+}
+
+// format number to 100,000, 10,000,000, 1,000,000,000, etc
+export function numberWithCommas(x: number) {
+  return x.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ",");
+}
