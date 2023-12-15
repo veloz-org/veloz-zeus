52da769f54c00cb080aa1a987840506e4a5b679e
Benrobo <alumonabenaiah71@gmail.com>
2023-12-16 00:43:40 +0100

    Fix API base URL and update lemonsqueezyConfig

---
+++ src/app/api/lemonsqueezy/store/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest } from "next/server";
+import sendResponse from "../../utils/sendResponse";
+import { RESPONSE_CODE } from "../../types";
+import env from "../../config/env";
+import CatchError from "../../utils/_error";
+import LemonsqueezyServices from "../../services/lemonsqeezy.service";
+
+const LQS = new LemonsqueezyServices();
+
+export const GET = CatchError(async (req: NextRequest) => {
+  // accessible only on development
+  if (process.env.NODE_ENV !== "development") {
+    return sendResponse.success(RESPONSE_CODE.SUCCESS, "Success", 200, {
+      msg: "This route is only accessible on development mode",
+    });
+  }
+
+  // check if lemonsqueezy api key is set
+  if (!env.LEMONSQUEEZY_API_KEY) {
+    return sendResponse.error(
+      RESPONSE_CODE.VALIDATION_ERROR,
+      "Lemonsqueezy API key is not set. Please set it in the .env file",
+      400
+    );
+  }
+
+  const stores = await LQS.getLemonsqueezyStores();
+
+  return sendResponse.success(RESPONSE_CODE.SUCCESS, "Success", 200, {
+    stores,
+  });
+});
