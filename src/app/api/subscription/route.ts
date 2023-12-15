a35b486cb86a929555c03f13e4f946f73c8ae39b
Benrobo <alumonabenaiah71@gmail.com>
2023-12-15 23:29:44 +0100

    updated

---
+++ src/app/api/subscription/route.ts
@@ -0,0 +1,12 @@
+import { NextRequest } from "next/server";
+import { isAuthenticated } from "../middlewares/auth";
+import CatchError from "../utils/_error";
+import SubscriptionController from "../controllers/subscription.controller";
+
+const subscriptionController = new SubscriptionController();
+
+export const POST = CatchError(
+  isAuthenticated(
+    async (req: NextRequest) => await subscriptionController.subscribe(req)
+  )
+);
