a04b77c3a53dbac4129ee5ed44114d486f5320dc
Benrobo <alumonabenaiah71@gmail.com>
2023-12-16 00:44:01 +0100

    Fix API base URL and update lemonsqueezyConfig

--- src/data/pricing/plan.ts
+++ src/data/pricing/plan.ts
@@ -14,8 +14,8 @@ export const pricingPlans = [
       currency: "USD",
       amount: 4,
     },
-    product_id: "",
-    variant_id: "",
+    product_id: 147184,
+    variant_id: 183156,
   },
   {
     id: "basic_104548",
@@ -26,8 +26,8 @@ export const pricingPlans = [
       currency: "USD",
       amount: 9,
     },
-    product_id: "",
-    variant_id: "",
+    product_id: 149209,
+    variant_id: 183213,
   },
   {
     id: "premium_123918",
@@ -38,7 +38,7 @@ export const pricingPlans = [
       currency: "USD",
       amount: 20,
     },
-    product_id: "",
-    variant_id: "",
+    product_id: 147501,
+    variant_id: 180561,
   },
 ] satisfies PricingPlan[];
