1e5dd437e463bb8969cc7e371b0a5c2dd55d8305
Benaiah <alumonabenaiah71@gmail.com>
2023-12-20 01:25:50 +0100

    Add GoogleProvider and callbacks to nextAuthOptions

--- src/app/api/auth/[...nextauth]/options.ts
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -1,7 +1,10 @@
 import type { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
 import prisma from "../../../../prisma/prisma";
 import PasswordManager from "../../utils/passwordManage";
+import HttpException from "../../utils/exception";
+import { RESPONSE_CODE } from "../../types";
 
 type CredentialsPayload = {
   email: string;
@@ -14,6 +17,7 @@ type CredentialsPayload = {
 export const nextAuthOptions: NextAuthOptions = {
   providers: [
     CredentialsProvider({
+      name: "credentials",
       credentials: {
         email: {},
         password: {},
@@ -49,8 +53,69 @@ export const nextAuthOptions: NextAuthOptions = {
         };
       },
     }),
+    GoogleProvider({
+      name: "google",
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
   ],
+  callbacks: {
+    async signIn({ user, account }) {
+      if (account?.provider === "google") {
+        const { email, image, name } = user;
+
+        // check if user exist
+        const accountWithGoogleAuth = await prisma.users.findFirst({
+          where: {
+            email: email as string,
+            provider: "google",
+          },
+        });
+
+        const accountWithoutGoogleAuth = await prisma.users.findFirst({
+          where: {
+            email: email as string,
+          },
+        });
+        const users = await prisma.users.findMany();
+
+        if (!accountWithGoogleAuth && !accountWithoutGoogleAuth) {
+          // create user
+          await prisma.users.create({
+            data: {
+              email: email as string,
+              username: name?.toLowerCase() as string,
+              fullname: name as string,
+              avatar: image as string,
+              auth_method: "oauth",
+              provider: "google",
+              role: users.length === 0 ? "admin" : "user",
+            },
+          });
+          return true;
+        }
+
+        if (accountWithGoogleAuth && !accountWithoutGoogleAuth) {
+          return true;
+        }
+
+        if (!accountWithGoogleAuth && accountWithoutGoogleAuth) {
+          throw new HttpException(
+            RESPONSE_CODE.USER_ALREADY_EXIST,
+            "OAuthAccountNotLinked",
+            400
+          );
+        }
+      }
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return `${baseUrl}/app/dashboard`;
+    },
+  },
+
   pages: {
     signIn: "/auth",
+    error: "/auth",
   },
 };
