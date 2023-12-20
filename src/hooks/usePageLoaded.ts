c57e8d1817e76959adcc2e2d8fe6d049b0083046
Benaiah <alumonabenaiah71@gmail.com>
2023-12-20 01:25:57 +0100

    Add usePageLoaded hook to check page loaded state

--- src/hooks/usePageLoaded.ts
+++ src/hooks/usePageLoaded.ts
@@ -1,5 +1,11 @@
 import React from "react";
 
+/**
+ *
+ * @param timeout : number (optional)
+ * @returns  boolean
+ * @description : This hook is used to check if the page is loaded or not. Specify timeout in milliseconds to delay the page loaded state. i.e 1000, not 1
+ */
 function usePageLoaded(timeout?: number) {
   const [pageLoaded, setPageLoaded] = React.useState(false);
 
