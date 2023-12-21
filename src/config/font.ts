1788118e578622ef937858f6caffc9343e32ce61
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 23:21:48 +0100

    Add Bricolage_Grotesque font to font.ts

--- src/config/font.ts
+++ src/config/font.ts
@@ -1,4 +1,4 @@
-import { Poppins } from "next/font/google";
+import { Poppins, Bricolage_Grotesque } from "next/font/google";
 
 export const ppReg = Poppins({
   subsets: ["latin"],
@@ -30,4 +30,11 @@ export const ppEB = Poppins({
   subsets: ["latin"],
   variable: "--font-ppEB",
   weight: ["900"],
+});
+
+// bricolage font
+export const blEB = Bricolage_Grotesque({
+  variable: "--font-blEB",
+  weight: ["800"],
+  subsets: ["latin", "latin-ext"],
 });
