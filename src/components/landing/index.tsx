f555e8c1bd7a97e4f411521e761c4b1d2e703b5f
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 20:33:18 +0100

    Add landing components

---
+++ src/components/landing/index.tsx
@@ -0,0 +1,4 @@
+export { default as FAQ } from "./faq";
+export { default as Features } from "./features";
+export { default as Header } from "./header";
+export { default as Pricing } from "./pricing";
