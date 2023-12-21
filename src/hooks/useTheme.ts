ae8e6bec607047ec080ffccbf9756c854b3ec1de
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 13:03:10 +0100

    Refactor useTheme hook to handle theme storage and initialization

--- src/hooks/useTheme.ts
+++ src/hooks/useTheme.ts
@@ -4,21 +4,38 @@ import { useEffect, useState } from "react";
 type Theme = "light" | "dark";
 
 export default function useTheme() {
-  const [theme, setTheme] = useState<Theme>(() => {
-    const storedTheme = window ? window.localStorage.getItem("theme") : null;
-    return (storedTheme as Theme) ?? ("light" as Theme);
-  });
+  const [theme, setTheme] = useState<Theme>();
 
   const toggleTheme = () => {
     setTheme((prevTheme) => (prevTheme === "dark" ? "light" : "dark"));
   };
 
   useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove("dark", "light");
-    root.classList.add(theme);
+    // Set theme on change
+    if (theme) {
+      const root = window.document.documentElement;
+      root.classList.remove("dark", "light");
+      root.classList.add(theme);
 
-    localStorage.setItem("theme", theme);
+      localStorage.setItem("theme", theme);
+    }
+  }, [theme]);
+
+  useEffect(() => {
+    // Set theme on first load
+    const root = window.document.documentElement;
+
+    if (!theme) {
+      const storedTheme =
+        localStorage.getItem("theme") === "undefined"
+          ? "light"
+          : localStorage.getItem("theme");
+
+      root.classList.remove("dark", "light", "undefined");
+      root.classList.add(storedTheme as Theme);
+
+      setTheme(storedTheme as Theme);
+    }
   }, [theme]);
 
   return { theme, setTheme, toggleTheme };
