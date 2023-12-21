838d6fa31e570f76ee27cd8028bb75da74fc5c49
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 20:33:49 +0100

    Add FAQ data

---
+++ src/data/landing/faq.ts
@@ -0,0 +1,33 @@
+// FAQ DATA
+export const FAQ_DATA = [
+  {
+    key: "faq1",
+    question: "What is a good way to learn React?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+  },
+  {
+    key: "faq2",
+    question: "How can I get access to the API docs?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+  },
+  {
+    key: "faq3",
+    question: "Is there any free trial period for the PRO version?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+  },
+  {
+    key: "faq4",
+    question: "Can I use Landrick for my clients?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+  },
+  {
+    key: "faq5",
+    question: "Do you have a money back guarantee?",
+    answer:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque.",
+  },
+];
