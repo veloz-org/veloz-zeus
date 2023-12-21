05166d4c23e4027a4b5f975284ebefe45d58740a
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 13:02:55 +0100

    Add landing header component

---
+++ src/components/landing/header/index.tsx
@@ -0,0 +1,25 @@
+import { FlexColCenter, FlexColStartCenter } from "@/components/Flex";
+import React from "react";
+import TrustedBy from "../testimonials/TrustedBy";
+
+function Header() {
+  return (
+    <FlexColCenter className="w-full h-full min-h-[400px] ">
+      <h1 className="text-5xl font-ppEB text-white-100">
+        Ship your startup in{" "}
+        <span className=" line-through text-3xl font-ppSB text-white-300 ">
+          Months
+        </span>{" "}
+        Weeks
+      </h1>
+      <p className="text-white-300 text-md font-ppReg">
+        Launch your startup with our startup launchpad saving development time
+        and cost.
+      </p>
+      <br />
+      <TrustedBy />
+    </FlexColCenter>
+  );
+}
+
+export default Header;
