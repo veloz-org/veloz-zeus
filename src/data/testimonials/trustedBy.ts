57f781e675c0b21fbcc407931b67506b854376ee
Benaiah <alumonabenaiah71@gmail.com>
2023-12-21 20:34:24 +0100

    Remove trustedBy.ts file

--- src/data/testimonials/trustedBy.ts
+++
@@ -1,9 +0,0 @@
-// trusted by data
-const TRUSTED_MAKERS = [
-  "/images/testimonials/trusted-by/1.jpeg",
-  "/images/testimonials/trusted-by/2.jpeg",
-  "/images/testimonials/trusted-by/3.jpeg",
-  "/images/testimonials/trusted-by/4.jpeg",
-];
-
-export default TRUSTED_MAKERS;
