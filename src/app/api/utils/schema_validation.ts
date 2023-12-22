5deacc77fed1c1cd30e50290c8e4ab67afa88f2e
Benaiah <alumonabenaiah71@gmail.com>
2023-12-22 11:29:43 +0100

    Add email format validation to addToWaitlistSchema

--- src/app/api/utils/schema_validation.ts
+++ src/app/api/utils/schema_validation.ts
@@ -64,5 +64,8 @@ export const addToWaitlistSchema = zod.object({
     .string({
       required_error: "Email is required",
     })
-    .email(),
+    .email()
+    .regex(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ),
 });
