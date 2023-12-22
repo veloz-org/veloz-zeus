dbabb997738a6d829a796ad0a5647e945f1a3b8d
Benaiah <alumonabenaiah71@gmail.com>
2023-12-22 11:29:47 +0100

    Add isAdmin middleware to DELETE route

--- src/app/api/waitlist/route.ts
+++ src/app/api/waitlist/route.ts
@@ -1,5 +1,5 @@
 import { NextRequest } from "next/server";
-import { isAuthenticated } from "../middlewares/auth";
+import { isAdmin, isAuthenticated } from "../middlewares/auth";
 import CatchError from "../utils/_error";
 import WaitlistController from "../controllers/waitlist.controller";
 
@@ -7,10 +7,21 @@ const waitlistController = new WaitlistController();
 
 export const GET = CatchError(
   isAuthenticated(
-    async (req: NextRequest) => await waitlistController.getWaitlist(req)
+    isAdmin(
+      async (req: NextRequest) => await waitlistController.getWaitlist(req)
+    )
   )
 );
 
 export const POST = CatchError(
   async (req: NextRequest) => await waitlistController.addToWaitlist(req)
+);
+
+export const DELETE = CatchError(
+  isAuthenticated(
+    isAdmin(
+      async (req: NextRequest) =>
+        await waitlistController.deleteFromWaitlist(req)
+    )
+  )
 );
