1a44e24dfd6778cc928165fc75b37294a6d97ca9
Benaiah <alumonabenaiah71@gmail.com>
2023-12-24 17:55:56 +0100

    added site data

---
+++ src/data/site.ts
@@ -0,0 +1,9 @@
+// your site config data
+const SITE_CONFIG = {
+  name: "Veloz",
+  headline: "Veloz - Your SaaS Launchpad",
+  description: "Ship faster with veloz.",
+  keywords: "boilerplate, starter kit, saucecode",
+};
+
+export default SITE_CONFIG;
