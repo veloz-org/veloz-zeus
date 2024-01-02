72430f1d6ed1f3b858c18431802357194d2a2631
Benaiah <alumonabenaiah71@gmail.com>
2024-01-02 05:28:28 +0100

    Refactor pricingPlans in plan.ts to include variants

--- src/data/pricing/plan.ts
+++ src/data/pricing/plan.ts
@@ -9,37 +9,73 @@ export const pricingPlans = [
     id: "lite_84507",
     name: "Lite",
     key: "lite",
-    duration: "monthly",
-    pricing: {
-      currency: "USD",
-      amount: 4,
-    },
+    variants: [
+      {
+        id: 183156,
+        duration: "monthly",
+        pricing: {
+          currency: "USD",
+          amount: 4,
+        },
+      },
+      {
+        id: 198725,
+        duration: "yearly",
+        pricing: {
+          currency: "USD",
+          amount: 4,
+        },
+      },
+    ],
     product_id: 147184,
-    variant_id: 183156,
   },
   {
     id: "basic_104548",
     name: "Basic",
     key: "basic",
-    duration: "monthly",
-    pricing: {
-      currency: "USD",
-      amount: 9,
-    },
+    variants: [
+      {
+        id: 183213,
+        duration: "yearly",
+        pricing: {
+          currency: "USD",
+          amount: 8,
+        },
+      },
+      {
+        id: 183213,
+        duration: "monthly",
+        pricing: {
+          currency: "USD",
+          amount: 9,
+        },
+      },
+    ],
     product_id: 149209,
-    variant_id: 183213,
     recommended: true,
   },
   {
     id: "premium_123918",
     name: "Premium",
     key: "premium",
-    duration: "monthly",
-    pricing: {
-      currency: "USD",
-      amount: 20,
-    },
+    variants: [
+      {
+        id: 180561,
+        duration: "monthly",
+        pricing: {
+          currency: "USD",
+          amount: 20,
+        },
+      },
+      {
+        id: 198727,
+        duration: "yearly",
+        pricing: {
+          currency: "USD",
+          amount: 108,
+        },
+      },
+    ],
     product_id: 147501,
-    variant_id: 180561,
   },
 ] satisfies PricingPlan[];
