30017c043a6f225e116083b506738fe9efa4b2a5
Benaiah <alumonabenaiah71@gmail.com>
2024-01-14 02:12:16 +0100

    Update API URL in axios.ts

--- src/http/axios.ts
+++ src/http/axios.ts
@@ -1,10 +1,11 @@
+import SITE_CONFIG from "@/data/site";
 import axios from "axios";
 
 // api url
 const API_URL =
   process.env.NODE_ENV === "development"
     ? "http://localhost:4000/api"
-    : "https://api.velozweb.dev";
+    : SITE_CONFIG.domain + "/api";
 
 // custom axios instance with necessary details
 const $axios = axios.create({
