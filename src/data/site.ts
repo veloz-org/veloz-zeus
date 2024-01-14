1be2a9cf4716182fcc0c6e945f73e64e98fec6aa
Benaiah <alumonabenaiah71@gmail.com>
2024-01-14 02:34:59 +0100

    Update site config with new name and headline

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,7 +1,7 @@
 // your site config data
 const SITE_CONFIG = {
-  name: "Veloz",
-  headline: "Veloz - Your saas launchpad",
+  name: "Zeus",
+  headline: "Zeus - veloz starter kit",
   description: "ship your saas in weeks, not months.",
   keywords: "boilerplate, starter kit, saucecode",
   domain: "https://veloz-zeus.tryveloz.com/",
