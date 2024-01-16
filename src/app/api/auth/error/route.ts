81bd910632c03ae7f22a13ee54be35b8d7d9cc16
Benaiah <alumonabenaiah71@gmail.com>
2024-01-16 01:52:43 +0100

    Fix import statement and remove unnecessary return statement

--- src/app/api/auth/error/route.ts
+++ src/app/api/auth/error/route.ts
@@ -1,8 +1,9 @@
+"use server";
 import { NextRequest } from "next/server";
 import { redirect } from "next/navigation";
 import CatchError from "../../utils/_error";
 
-export const GET = CatchError((req: NextRequest) => {
+export const GET = (req: NextRequest) => {
   if (req.method === "GET") {
     console.log("AUTH ERROR");
     const error = req.nextUrl.searchParams.get("error");
@@ -10,6 +11,6 @@ export const GET = CatchError((req: NextRequest) => {
     const encodedError = encodeURIComponent(error as string);
 
     // redirect to auth page
-    return redirect(`/auth?error=${encodedError}`);
+    redirect(`/auth?error=${encodedError}`);
   }
-});
+};
